2fe8bba4ae71d87d5224229c2a5bc29737649dce
You Gwang Ho <ykh395987@gmail.com>
2026-09-21 14:08:11 +0900

    Update data.py

--- app/data.py
+++ app/data.py
@@ -1,1 +1,23 @@
+from pathlib import Path
+
+import pandas as pd
+import streamlit as st
+
+BASE_DIR = Path(**file**).resolve().parents[1]
+DATA_DIR = BASE_DIR / "data"
+
+@st.cache_data
+def load_policy_data():
+file_path = DATA_DIR / "policies.csv"
+return pd.read_csv(file_path)
+
+@st.cache_data
+def load_signal_data():
+file_path = DATA_DIR / "signals.csv"
+return pd.read_csv(file_path)
+
+@st.cache_data
+def load_organizations():
+file_path = DATA_DIR / "organizations.csv"
+return pd.read_csv(file_path)
 
